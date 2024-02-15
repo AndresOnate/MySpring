99c10e840997b389cbdf1092b25ca0603d794779
unknown <andres.onate@labinfo.is.escuelaing.edu.co>
2024-02-15 14:32:30 -0500

    Soring

---
+++ src/main/java/org/example/InvokeMain.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import java.lang.reflect.*;
+import java.util.Arrays;
+
+public class InvokeMain {
+
+    public static void main(String... args) {
+        try {
+            Class<?> c = Class.forName(args[0]);
+            Class[] argTypes = new Class[]{int.class, int.class};
+            Method add = c.getDeclaredMethod("add", argTypes);
+            String[] mainArgs = Arrays.copyOfRange(args, 1, args.length);
+            System.out.format("invoking %s.main()%n", c.getName());
+            add.invoke(null, Integer.parseInt(mainArgs[0]), Integer.parseInt(mainArgs[1]));
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class c  = Class.forName("org.example.Student");
+        Field[] fields = c.getFields();
+        printMembers(fields, "Campos");
+        Constructor[] constructors = c.getConstructors();
+        printMembers(constructors, "Constructores");
+        printMembers(fields, "Campos");
+        Method[] methods = c.getMethods();
+        printMembers(methods, "Métodos");
+    }
+
+    private static void printMembers(Member[] mbrs, String s) {
+        out.format("%s:%n", s);
+        for (Member mbr : mbrs) {
+            if (mbr instanceof Field) {
+                out.format(" %s%n", ((Field) mbr).toGenericString());
+            }else if (mbr instanceof Constructor) {
+                out.format(" %s%n", ((Constructor) mbr).toGenericString());
+            }else if (mbr instanceof Method) {
+                out.format(" %s%n", ((Method) mbr).toGenericString());
+            }
+        }
+        if (mbrs.length == 0) {
+            out.format(" -- No %s --%n", s);
+        }
+        out.format("%n");
+    }
+
+    public static void add(int a, int b){
+        System.out.println("a+b: " + (a+b));
+    }
+
+}
